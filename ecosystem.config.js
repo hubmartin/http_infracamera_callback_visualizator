8f6613753990b1654a010f22916685a757e882ad
Ubuntu <ubuntu@ip-172-26-3-166.eu-central-1.compute.internal>
2020-11-15 17:51:30 +0100

    Add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'main.js',
+    name: 'infrared',
+    watch: '.',
+    ignore_watch : ["node_modules", "public"],
+  },],
+
+  
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};

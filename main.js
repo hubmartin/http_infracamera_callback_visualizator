9149954700f3eaaf6103a400f39faa9c95e3edc5
hubmartin <hub.martin@gmail.com>
2020-11-15 14:25:20 +0100

    Working draft

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 var app = require('express')();
+var bodyParser = require('body-parser')
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+/*
+curl -X POST -H "Content-Type: application/json" --data @payload.json localhost:3010/post
+*/
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+
+    app.get('/callback', (req, res) => {
+        io.emit('infrared', {"json": true});
+        res.send('OK');
+        });
+    
+  });
+
+app.post('/post', (req, res) => {
+    console.log('Got body:', req.body);
+    io.emit('infrared', req.body);
+    res.sendStatus(200);
 });
 
 http.listen(3010, () => {
